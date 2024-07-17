ab8373961442fa913cec67bca094ec97427ce5a5
nasquez23 <ivannaskov15@gmail.com>
2024-07-17 19:49:53 +0200

    Add repositories

---
+++ backend/src/main/java/org/buytopia/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package org.buytopia.repositories;
+
+import org.buytopia.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
