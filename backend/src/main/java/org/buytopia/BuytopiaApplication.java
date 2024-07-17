9a37d70efdedb6ee5debdfd36f8717ba3ad30487
nasquez23 <ivannaskov15@gmail.com>
2024-07-17 13:46:05 +0200

    Add Spring Boot app

---
+++ backend/src/main/java/org/buytopia/BuytopiaApplication.java
@@ -0,0 +1,13 @@
+package org.buytopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuytopiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuytopiaApplication.class, args);
+    }
+
+}
